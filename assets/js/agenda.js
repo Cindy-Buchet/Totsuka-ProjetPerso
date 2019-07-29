f98aa1bd37f2f2e2b7a20199cd170e929686881e
Cindy Buchet <mllcindy@hotmail.com>
2019-07-29 15:58:47 +0200

    modification code agenda

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -4,67 +4,140 @@ fetch('https://spreadsheets.google.com/feeds/list/1m_yWTlrWu7xhaEXh5QCSk8HpppKr_
     
     let mydata = value;
     let joursTab = mydata.feed.entry;
-    console.log(joursTab);
-
-  function func1(a) {
-    for (let i = 0; i < joursTab.length; i++){
+    //console.log(joursTab);
         
-        if(arguments[0] == joursTab[i].gsx$jour.$t){
-        let gsvar = joursTab[i].gsx$jour.$t;
-        
-    }
-    }
-  }
-
-  function GetActivities(a) {
-    for (let i = 0; i < joursTab.length; i++){
-        
-        if(arguments[0] == joursTab[i].gsx$jour.$t){
-        let act1 = joursTab[i].gsx$activite1.$t;
-        let act2 = joursTab[i].gsx$activite2.$t;
-        let act3 = joursTab[i].gsx$activite3.$t;
-        
-        return act1,act2,act3;
-    }
-    }
-  }
-
-  
-  
-  function func2(a) {
-    let selectClass = document.querySelectorAll(".jours");
-    for (let z = 0; z < selectClass.length; z++){
-        if((arguments[0]) == selectClass[z].innerHTML){
-        
-        let calendarvar = selectClass[z].innerHTML;
-        
-        }
-        }
+        //récuprération du mois via la case du sheet fixé manuellement
+        let mois = joursTab[0].gsx$mois.$t;
+        //console.log(mois);
     
-    }
-    
-    let selectClass = document.querySelectorAll(".jours");
-    for (let r = 0; r < selectClass.length; r++){
+        //création du tab qui va contenir les ["jour","activite1","activite2","activité3"]
+        let TabMembers = [];
         
-        let var1 = func1(r);
-        let var2 = func2(r);
-        let activite = GetActivities(r);
+        for (let m = 0; m<joursTab.length; m++){
+            //recupération du jours
+            let day = joursTab[m].gsx$jour.$t;
+            
+            //recupération des infos de l'activité 1
+            let acti1 = joursTab[m].gsx$activite1.$t;
+            let heure1 = joursTab[m].gsx$heure1.$t;
+            let orga1 = joursTab[m].gsx$par1.$t;
 
-        if (var1 == var2){
-            let newP = document.createElement('p');
-            newP.innerHTML = activite;
-            selectClass[r].appendChild(newP);
+            //récupération des infos de l'activité 2
+            let acti2 = joursTab[m].gsx$activite2.$t;
+            let heure2 = joursTab[m].gsx$heure2.$t;
+            let orga2 = joursTab[m].gsx$par2.$t;
+
+            //récupération des infos de l'activité 3
+            let acti3 = joursTab[m].gsx$activite3.$t;
+            let heure3 = joursTab[m].gsx$heure2.$t;
+            let orga3 = joursTab[m].gsx$par3.$t;
+
+            //création du tab pour récupérer tout les infos en un tab
+            let NewTab = [];
+            
+            //push du jour
+            NewTab.push(day); //TabMembers[index][0]
+
+            function PushTab(donnee1, donnee2, donnee3){
+                NewTab.push(donnee1); // Activité 
+                NewTab.push(donnee2); // Heure
+                NewTab.push(donnee3); // Par
+            }
+            PushTab(acti1, heure1, orga1);
+            PushTab(acti2, heure2, orga2);
+            PushTab(acti3, heure3, orga3);
+
+            TabMembers.push(NewTab);
 
         }
         
+        //parcours du tableau et création des cards
+        for(index = 0; index < TabMembers.length; index++){
+            
+            //on affiche que les jours ou il y a des activités
+            if ( TabMembers[index][1] == "" && TabMembers[index][2] == "" && TabMembers[index][3] == "" ){
+                //possibilité d'afficher "aucune activité" sur le jours"
+                let Cardcontainer = document.getElementById('card-container');
+            
 
 
-    }
+            let col = document.createElement('div');
+            col.className = 'col-12 col-md-4 col-lg-3';
 
+            let card = document.createElement('div');
+            card.className = 'card bg-light mb-3';
 
-    
-  
+            
 
+            let cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            //Card Title : ici jour + mois 
+            let title = document.createElement('h5');
+            title.innerText = TabMembers[index][0] + " " +  mois;
+            title.className = 'card-header text-center';
+
+            let text = document.createElement('p');
+            text.innerText = "\n Aucune activité n'est organisé pour ce jour.";
+            text.className = 'card-text text-center';
+            
+            
+            card.appendChild(title);
+            cardBody.appendChild(text);
+            card.appendChild(cardBody);
+            col.appendChild(card);
+            Cardcontainer.appendChild(col);
+            }else{
+
+                // on vérifie qu'il y a une activité et seulement si c'est le  cas on affiche la card
+                if (TabMembers[index][1] != "" && TabMembers[index][2] != "" && TabMembers[index][3] != "" ) {
+                let Cardcontainer = document.getElementById('card-container');
+                
+
+                let col = document.createElement('div');
+                col.className = 'col-12 col-md-4 col-lg-3';
+
+                let card = document.createElement('div');
+                card.className = 'card bg-light mb-3';
+
+                
+
+                let cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+
+                //Card Title : ici jour + mois 
+                let title = document.createElement('h5');
+                title.innerText = TabMembers[index][0] + " " +  mois;
+                title.className = 'card-header text-center';
+                card.appendChild(title);
+
+                let ListUl = document.createElement("ul");
+
+                function CreatLi(Tab1, Tab2, Tab3){
+                    let text = document.createElement('li');
+                    text.innerHTML = "<p><span>" + Tab1 + "</span> à " + Tab2 + "</p><p>Organisé par " + Tab3 + "</p>";
+                    ListUl.appendChild(text); 
+                }
+                
+                CreatLi(TabMembers[index][1], TabMembers[index][2],TabMembers[index][3]); 
+
+                if (TabMembers[index][4] != "" && TabMembers[index][5] != "" && TabMembers[index][6] != "" ){
+                    CreatLi(TabMembers[index][4], TabMembers[index][5],TabMembers[index][6]); 
+                }
+                
+                if (TabMembers[index][7] != "" && TabMembers[index][8] != "" && TabMembers[index][9] != "" ){
+                    CreatLi(TabMembers[index][7], TabMembers[index][8],TabMembers[index][9]); 
+                }
+
+                cardBody.appendChild(ListUl);
+                
+                card.appendChild(cardBody);
+                col.appendChild(card);
+                Cardcontainer.appendChild(col);
+                }
+        }
+                
+        }
 }).catch(error => {
     console.log(error)
 })
